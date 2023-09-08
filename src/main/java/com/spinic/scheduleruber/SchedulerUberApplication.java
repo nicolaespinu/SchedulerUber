93980496cb3cfe159fe2feef113ffc674017a1f7
Nicolae Spinu <11spinic@gmail.com>
2023-09-08 22:10:56 +0300

    Init commit

---
+++ src/main/java/com/spinic/scheduleruber/SchedulerUberApplication.java
@@ -0,0 +1,13 @@
+package com.spinic.scheduleruber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerUberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerUberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spinic/scheduleruber/SchedulerUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spinic.scheduleruber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerUberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
